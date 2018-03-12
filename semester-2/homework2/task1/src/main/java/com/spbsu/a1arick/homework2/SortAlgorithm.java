216b898997743710b44c4a522f9ae728c3da5593
a1arick <algsavelev@gmail.com>
2018-03-12 17:42:05 +0300

    homework 2.1

---
+++ semester-2/homework2/task1/src/main/java/com/spbsu/a1arick/homework2/SortAlgorithm.java
@@ -0,0 +1,5 @@
+package com.spbsu.a1arick.homework2;
+
+public interface SortAlgorithm {
+    void Sort(int[] a);
+}
---
+++ semester-2/homework2/task1/src/test/java/SortAlgorithmTest.java
@@ -0,0 +1,41 @@
+import com.spbsu.a1arick.homework2.BubleSort;
+import com.spbsu.a1arick.homework2.Qsort;
+import com.spbsu.a1arick.homework2.SortAlgorithm;
+import org.junit.Test;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.junit.Assert.assertTrue;
+
+public class SortAlgorithmTest {
+    @Test
+    public void bubble() {
+        for (int i = 0; i < 10; i++) {
+            assertTrue(test(new BubleSort()));
+        }
+    }
+
+    @Test
+    public void qSort() {
+        for (int i = 0; i < 10; i++) {
+            assertTrue(test(new Qsort()));
+        }
+    }
+
+    private static boolean test(SortAlgorithm sortAlgorithm){
+        int n = 1000;
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++){
+            a[i] = ThreadLocalRandom.current().nextInt();
+        }
+
+        sortAlgorithm.Sort(a);
+
+        for (int i = 1; i < n; i++){
+            if (a[i-1] > a[i])
+                return false;
+        }
+        return true;
+    }
+}
