4b4d58378624eb0a53fddc5a9121beb1940eedb4
a1arick <algsavelev@gmail.com>
2018-04-05 21:15:14 +0300

    Homework 5 task 1

---
+++ semester-2/homework5/task1/src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
+
+public class Controller {
+    @FXML
+    private Slider slider;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    public void initialize() {
+        slider.valueProperty().addListener((observable, oldValue, newValue) -> progressBar.progressProperty().setValue(newValue));
+    }
+}
