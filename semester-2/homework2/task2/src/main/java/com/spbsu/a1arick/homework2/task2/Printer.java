9bb9e2cb012b6384dec97f37c7ae5cb510fbf476
Timofey Bryksin <timofey.bryksin@gmail.com>
2018-06-04 19:27:50 +0300

    Revert "homework 2.2"

--- semester-2/homework2/task2/src/main/java/com/spbsu/a1arick/homework2/task2/Printer.java
+++
@@ -1,7 +0,0 @@
-package com.spbsu.a1arick.homework2.task2;
-
-import java.io.IOException;
-
-public interface Printer {
-    void print(int[][] a) throws IOException;
-}
--- semester-2/homework2/task2/src/main/java/com/spbsu/a1arick/homework2/task2/SpiralAlgorithmConsole.java
+++
@@ -1,9 +0,0 @@
-package com.spbsu.a1arick.homework2.task2;
-
-public class SpiralAlgorithmConsole extends AbstractSpiralAlgorithm {
-
-    @Override
-    public void print(int[][] a) {
-        printToStream(a, System.out);
-    }
-}
